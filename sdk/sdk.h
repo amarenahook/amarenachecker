b13685828cedc2bc09039d1ee41ed1f2e7ade774
amarenaspace <33689345+amarenahook@users.noreply.github.com>
2017-11-15 23:27:22 +0100

    Update sdk.h

--- sdk/sdk.h
+++ sdk/sdk.h
@@ -2,3 +2,9 @@
 
 #include <windows.h>
 #include "vmt.h"
+
+extern VMTHook* vmt_clientmode;
+extern VMTHook* vmt_client;
+extern VMTHook* vmt_engine;
+extern VMTHook* vmt_surface;
+extern VMTHook* vmt_panel;
